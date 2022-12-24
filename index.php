100f12779b6741ffec9ee920c2339ff7eee83b9f
amilashanaka <dsa.amilashanaka@gmail.com>
2022-12-24 05:51:26 +0000

    update

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include_once('headder.php') ?>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        <?php include_once('./nav_bar.php'); ?>
+        <!-- Main Sidebar Container -->
+        <?php include_once('./side_bar.php'); ?>
+
+
+        <?php include_once('page.php');?>
+
+        <?php include_once('control_side_bar.php');?>
+
+        <?php include_once('footer.php');?>
+</body>
+
+</html>
